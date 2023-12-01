0e7dc5fc40c289ea1c8d865f1621806a8893292a
Afonso Oliveira <afe.oliveira@campus.fct.unl.pt>
2023-12-01 14:26:34 +0000

    Create add_layers function, to extract the bands from an image and store in a dictionary.

--- src/container/loader.py
+++ src/container/loader.py
@@ -1,7 +1,20 @@
-class ImageRaster:
+from osgeo import gdal
 
-    data = None
+class MapRaster:
+
+    layers = {}
+
+    transform = None
+    projection = None
 
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        self.layers = {}
+        self.transform = None
+        self.projection = None
+
+    def add_layers(self, path: str, names: list[str]):
+        dataset = gdal.Open(path, gdal.GA_ReadOnly)
+
+        for i in range(1, dataset.RasterCount + 1):
+            band = dataset.GetRasterBand(i).ReadAsArray()
+            self.layers[names[i - 1]] = band
