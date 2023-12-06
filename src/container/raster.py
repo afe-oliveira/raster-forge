7bef61b8e5249947fa738bf9b3ed7e302dc61d53
Afonso Oliveira <afe.oliveira@campus.fct.unl.pt>
2023-12-06 20:14:27 +0000

    Add addition and removing of layers from Raster class. Initial setup of the pyproj.toml file.

--- src/container/raster.py
+++ src/container/raster.py
@@ -12,13 +12,12 @@ class RasterImportConfig(TypedDict):
 
 
 class Raster:
-
     layers = {}
 
     transform = None
     projection = None
 
-    def __init__(self, name, age):
+    def __init__(self):
         self.layers = {}
         self.transform = None
         self.projection = None
@@ -27,7 +26,14 @@ class Raster:
         dataset = gdal.Open(path, gdal.GA_ReadOnly)
 
         for item in config:
+            band = dataset.GetRasterBand(item["id"]).ReadAsArray()
+            band = np.clip(band, item["min"], item["max"])
+            self.layers[item["name"]] = band
 
-            band = dataset.GetRasterBand(item['id']).ReadAsArray()
-            band = np.clip(band, item['min'], item['max'])
-            self.layers[item['name']] = band
+    def add_layer(self, data: np.ndarray, name: str):
+        if name not in self.layers.keys():
+            self.layers[name] = data
+
+    def remove_layer(self, name: str):
+        if name in self.layers.keys():
+            self.layers.pop(name)
