9625b63a9d80905918953de3c470a65968f0fd9b
Afonso Oliveira <afe.oliveira@campus.fct.unl.pt>
2023-12-20 13:41:13 +0000

    Add layer edit functionality; Add raster typing hints; Start layer scaling functionality.

--- ProjectNabu/container/raster.py
+++ ProjectNabu/container/raster.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypedDict, Union, Literal, Dict, Optional, Tuple
 
 import numpy as np
 import rasterio
@@ -7,30 +7,55 @@ import rasterio
 class RasterImportConfig(TypedDict):
     id: int
     name: str
-    min: int
-    max: int
+    type: Literal['relative', 'absolute']
+
+
+class LayerFormat(TypedDict):
+    data: np.ndarray[Union[np.uint8, np.int32]]
+    type: Literal['relative', 'absolute']
 
 
 class Raster:
 
-    __scale = None
+    layers: Dict[str, np.ndarray[Union[np.uint8, np.int32]]] = {}
 
-    layers = {}
-    transform = None
-    projection = None
+    _scale: int = None
+    _transform: Optional[Tuple[float, float, float, float, float, float]] = None
+    _projection = Optional[str] = None
+    _metadata = Optional[Dict[str, str]] = None
 
     def __init__(self, scale: int):
-        self.__scale = scale
-
-        self.layers = {}
-        self.transform = None
-        self.projection = None
+        self._scale = scale
 
     # ****************
 
     @property
     def scale(self):
-        return self.__scale
+        return self._scale
+
+    @property
+    def transform(self) -> Optional[Tuple[float, float, float, float, float, float]]:
+        return self._transform
+
+    @transform.setter
+    def transform(self, value: Optional[Tuple[float, float, float, float, float, float]]):
+        self._transform = value
+
+    @property
+    def projection(self) -> Optional[str]:
+        return self._projection
+
+    @projection.setter
+    def projection(self, value: Optional[str]):
+        self._projection = value
+
+    @property
+    def metadata(self) -> Optional[Dict[str, str]]:
+        return self._metadata
+
+    @metadata.setter
+    def metadata(self, value: Optional[Dict[str, str]]):
+        self._metadata = value
 
     # ****************
 
@@ -38,6 +63,13 @@ class Raster:
         with rasterio.open(path) as dataset:
             for item in config:
                 band = dataset.read(item["id"])
+
+                if item['type'] == 'relative':
+                    band = np.interp(band, (band.min(), band.max()), (0, 255))
+                    self.layers[item["name"]] = band.astype(np.uint8)
+                elif item['type'] == 'absolute':
+                    pass
+
                 band = np.interp(band, (band.min(), band.max()), (item['min'], item['max']))
                 self.layers[item["name"]] = band.astype(np.uint8)
 
@@ -48,3 +80,7 @@ class Raster:
     def remove_layer(self, name: str):
         if name in self.layers.keys():
             self.layers.pop(name)
+
+    def edit_layer(self, current_name: str, new_name: str):
+        if current_name in self.layers.keys():
+            self.layers[new_name] = self.layers.pop(current_name)
