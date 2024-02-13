51207ef7b31adfa6bcb4d5ab8ee2e3529b412ee5
Afonso Oliveira <afe.oliveira@campus.fct.unl.pt>
2024-02-13 17:07:40 +0000

    Fix index tests.

--- tests/fixtures/fixtures_index.py
+++ tests/fixtures/fixtures_index.py
@@ -7,7 +7,7 @@ with open("tests/files/index.pkl", "rb") as file:
     parameters = pickle.load(file)
 
 
-@pytest.fixture(params=[])
+@pytest.fixture(params=parameters)
 def data_index(request):
     """Fixture that defines the raster initialization data to be tested."""
     return request.param
