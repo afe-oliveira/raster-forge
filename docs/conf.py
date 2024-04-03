abc99cd7e2298aebcc7d60abd97208a909cafd73
Afonso Oliveira <afe.oliveira@campus.fct.unl.pt>
2024-04-03 15:36:43 +0100

    Updata documentation theme.

--- docs/conf.py
+++ docs/conf.py
@@ -33,30 +33,25 @@ napoleon_include_private_with_doc = False
 
 # HTML Output
 
-html_theme = "sphinx_material"
+html_theme = "pydata_sphinx_theme"
 
 html_title = "Raster Forge"
 
-html_logo = "_static/logo-flat.svg"
 html_favicon = "_static/logo-flat.svg"
-
-html_theme_options = {
-    "nav_title": "Raster Forge",
-    "base_url": "https://afe-oliveira.github.io/raster-forge/",
-    "color_primary": "yellow",
-    "color_accent": "teal",
-    "repo_type": "github",
-    "repo_url": "https://github.com/afe-oliveira/raster-forge",
-    "repo_name": "raster-forge",
-    "globaltoc_depth": 5,
-    "globaltoc_collapse": True,
-    "globaltoc_includehidden": True,
-}
 
 html_last_updated_fmt = ""
 
-html_sidebars = {
-    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+html_theme_options = {
+    "logo": {
+        "alt_text": "Raster Forge",
+        "image_light": "_static/logo-flat.svg",
+        "image_dark": "_static/logo-flat-white.svg",
+    },
+    "navbar_align": "left",
+    "show_prev_next": False,
 }
 
 html_static_path = ["_static"]
+html_css_files = [
+    'css/custom.css',
+]
